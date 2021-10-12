03cd34e8d00f7e24c4750f93197fb5aa7e9f1f2f
otienovicto <90698830+otienovicto@users.noreply.github.com>
2021-10-12 16:53:29 +0300

    Add files via upload

---
+++ simple calculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2,sum,diff,product,mod;
+ float quot;
+ num1=200;
+ num2=56;
+ sum=num1+num2;
+ diff=num1-num2;
+ product=num1*num2;
+ quot=num1/num2;
+ mod=num1%num2;
+ printf("The sum is %d\n",sum);
+ printf("The difference is %d\n",diff);
+ printf("The product is %d\n",product);
+ printf("The quot is %f\n",quot);
+ printf("The modulas is %d\n",mod);
+    return 0;
+}
